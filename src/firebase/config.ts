340fed8cfa806789720ff2837c958627b7df9785
Resmoke Services <backendresmoke@gmail.com>
2025-12-28 20:15:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8803512383-d64d8",
+  "appId": "1:304611836628:web:b39fe4a9ca0ac8a97d3141",
+  "apiKey": "AIzaSyCZ6QnwwrD4UNDPmQJGrwk6HPdqBmY6g_g",
+  "authDomain": "studio-8803512383-d64d8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "304611836628"
+};
