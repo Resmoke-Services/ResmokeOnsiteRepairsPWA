5517f6534f77ce64455a1eb7db09c5737a8d09dd
Resmoke Services <backendresmoke@gmail.com>
2025-12-28 20:22:15 +0000

    place these secure in a .env file:
    apiKey: "AIzaSyCM30raXoyrXvikSaV-wvP

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-8803512383-d64d8",
-  "appId": "1:304611836628:web:b39fe4a9ca0ac8a97d3141",
-  "apiKey": "AIzaSyCZ6QnwwrD4UNDPmQJGrwk6HPdqBmY6g_g",
-  "authDomain": "studio-8803512383-d64d8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "304611836628"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
